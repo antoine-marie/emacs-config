689c29575307e753b632b9b66479a73f9f8cc329
antoine-marie <antoine.marie@ens-lyon.fr>
2021-01-16 11:44:40 +0100

    error in init.el

--- init.el
+++ init.el
@@ -1,6 +1,28 @@
-(setq-default LaTeX-equation-label "eq:")
-(setq-default LaTeX-eqnarray-label "eq:")
-(setq-default LaTeX-amsmath-label "eq:")
-(setq-default LaTeX-float "h")
-(setq-default LaTeX-figure-label "fig:")
-(setq-default LaTeX-table-label "tab:")
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company-auctex popup-kill-ring doom-themes zenburn-theme rainbow-delimiters auctex-latexmk avy org-bullets beacon darkburn-theme which-key use-package)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "1ASC" :family "xos4 Terminus")))))
